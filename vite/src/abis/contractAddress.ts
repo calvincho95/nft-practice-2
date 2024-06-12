a78811651fc7a298aeb4496e146aaf9be1634a34
calvincho95 <calvincho95@naver.com>
2024-06-12 19:17:19 +0900

    MINTNFT Practice

--- vite/src/abis/contractAddress.ts
+++ vite/src/abis/contractAddress.ts
@@ -1,2 +1,2 @@
 export const mintContractAddress = "0x876dB5dD8efe87595C84C3401BCe419E353Fc0fe";
-export const saleContractAddress = "0xd9145CCE52D386f254917e481eB44e9943F39138";
+export const saleContractAddress = "0x8948cD4062B349841dce3AE727C3E01852248616";
